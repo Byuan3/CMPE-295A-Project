dede72aa38ac2699b4c337ce82a378ed6e8e53e7
Byuan3 <byuan@ucsd.edu>
2022-10-21 14:42:14 -0700

    Update SocketClient.py

--- SocketClient.py
+++ SocketClient.py
@@ -1,21 +1,8 @@
-import socket
-
-
-def send_message(message):
-    host = socket.gethostname()
-    port = 9998
-
-    client_socket = socket.socket()
-    client_socket.connect((host, port))
-
-    client_socket.send(message.encode())
-    print('Send message to Server: ' + message)
-
-    data = client_socket.recv(1024).decode()
-    print('Received from server: ' + data)
-
-    client_socket.close()
+import helloworldtockenpackage as hw
 
 
 if __name__ == '__main__':
-    send_message("Hello World")
+    while True:
+        message = input('Message --> ')
+        print('Sending ...')
+        hw.send_message(message)
