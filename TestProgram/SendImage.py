f3be014b447c69ad4beb922e1b1e50bb1718b01a
Byuan3 <byuan@ucsd.edu>
2022-11-09 01:52:37 -0800

    Update SendImage.py

--- TestProgram/SendImage.py
+++ TestProgram/SendImage.py
@@ -1,5 +1,12 @@
 import TPUnityAI as tp
+import cv2
+
+
+def cut_resolution_by(image, factor):
+    imageResize = cv2.resize(image, (image.shape[1] // factor, image.shape[0] // factor))
+    return imageResize
 
 
 if __name__ == '__main__':
-    tp.send_image(r"..\Images\IMG_4637.jpg")
+    image = cv2.imread(r"..\Images\IMG_4637.jpg")
+    tp.send_image(cut_resolution_by(image, 8))
